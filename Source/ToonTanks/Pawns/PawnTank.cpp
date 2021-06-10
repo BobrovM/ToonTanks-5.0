48048e06f0e13c1c09e33a1ed564240a02bdcbf0
Mikhail Bobrov <45899107+BobrovM@users.noreply.github.com>
2021-06-11 01:02:14 +0300

    Initial commit

---
+++ Source/ToonTanks/Pawns/PawnTank.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PawnTank.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+APawnTank::APawnTank()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm"));
+	SpringArmComponent->SetupAttachment(RootComponent);
+
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	CameraComponent->SetupAttachment(SpringArmComponent);
+}
+
+// Called when the game starts or when spawned
+void APawnTank::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+// Called every frame
+void APawnTank::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APawnTank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
